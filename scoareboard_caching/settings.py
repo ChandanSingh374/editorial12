a6a5db66fc9013894dd6843d42a41b92e0524276
chandan.singh@scaler.com <chandan.singh@scaler.com>
2022-12-02 15:01:24 +0530

    hello

--- scoareboard_caching/settings.py
+++ scoareboard_caching/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-#1$51hg#!%s@)!*bb23%j6-@2v90*+y9zl$*r6%e^m!b*zdah6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
