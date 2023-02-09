1c771b6c53e0284f7f0a7cb40b6330331ed648f7
ChandanSingh374 <87111734+ChandanSingh374@users.noreply.github.com>
2023-02-09 14:14:37 +0530

    Update tests.py

--- cohorts/tests.py
+++ cohorts/tests.py
@@ -22,49 +22,46 @@ class GetScoreBoardTests(APITestCase):
     def test_should_return_200(self):
         url = reverse('cohort_scoreboard', args=[1])
         response = self.make_new_request(url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, "API should return success response")
 
-    def test_empty_result_if_score_not_updated(self):
+    def test_when_no_score_updated(self):
         url = reverse('cohort_scoreboard', args=[1])
         response = self.make_new_request(url)
 
         res_body = json.loads(response.content)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(res_body), 0)
+        self.assertEqual(len(res_body), 0, "Should return empty result")
+        self.check_cost_expectation(100, 200, "Should hit DB to get detail")
 
-    def test_should_return_data_if_score_updated(self):
+    def test_non_empty_result_when_non_zero_score_exists_for_cohort_user(self):
         url = reverse('cohort_scoreboard', args=[1])
         self.update_score(1, 1, 100)
         response = self.make_new_request(url)
 
         res_body = json.loads(response.content)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(res_body), 1)
-        self.assertEqual(res_body[0].get("email"), 'Cohort1+user1@example.com')
-        self.assertEqual(res_body[0].get("score"), 100)
-        self.check_cost_expectation(100, 200)
+        self.assertEqual(len(res_body), 1, "Should return non empty result")
+        self.assertEqual(res_body[0].get("email"), 'Cohort1+user1@example.com', "Validate Returned Result")
+        self.assertEqual(res_body[0].get("score"), 100, "Validate Returned Score")
+        self.check_cost_expectation(100, 200, "Should hit DB to get detail")
 
-    def test_for_second_request_for_same_cohort_should_fetch_from_cache(self):
+    def test_second_request_for_same_cohort_fetched_from_cache(self):
         url = reverse('cohort_scoreboard', args=[1])
         self.update_score(1, 1, 100)
         response_1 = self.make_new_request(url)
         
         # 1st Request Expectation
         res_1_body = json.loads(response_1.content)
-        self.assertEqual(response_1.status_code, 200)
-        self.assertEqual(len(res_1_body), 1)
-        self.check_cost_expectation(100, 200)
+        self.assertEqual(len(res_1_body), 1, "Should return non empty result")
+        self.check_cost_expectation(100, 200, "1st fetch should hit DB to get detail")
         
         # Second Request
         response_2 = self.make_new_request(url)
 
         # 2nd Request Expectation
         res_2_body = json.loads(response_2.content)
-        self.assertEqual(response_2.status_code, 200)
-        self.assertEqual(len(res_2_body), 1)
-        self.check_cost_expectation(1, 50)
+        self.assertEqual(len(res_2_body), 1, "Should return non empty result")
+        self.check_cost_expectation(1, 50, "2nd fetch should hit cache to get detail")
 
-    def test_should_set_cohort_level_cache(self):
+    def test_cohort_level_cache(self):
         # Update Cache For 1st cohort
         url = reverse('cohort_scoreboard', args=[1])
         self.update_score(1, 1, 100)
@@ -72,9 +69,8 @@ class GetScoreBoardTests(APITestCase):
         
         # 1st Request cohort 1 Expectation
         res_1_cohort_1_body = json.loads(response_1_cohort_1.content)
-        self.assertEqual(response_1_cohort_1.status_code, 200)
-        self.assertEqual(len(res_1_cohort_1_body), 1)
-        self.check_cost_expectation(100, 200)
+        self.assertEqual(len(res_1_cohort_1_body), 1, "Should return non empty result for batch 1")
+        self.check_cost_expectation(100, 200, "1st fetch for batch 1, should hit DB to get detail")
 
         # Update Cache For 2nd cohort
         url = reverse('cohort_scoreboard', args=[2])
@@ -85,8 +81,8 @@ class GetScoreBoardTests(APITestCase):
         
         # 1st Request cohort 2 Expectation
         res_1_cohort_2_body = json.loads(response_1_cohort_2.content)
-        self.assertEqual(len(res_1_cohort_2_body), 3)
-        self.check_cost_expectation(100, 200)
+        self.assertEqual(len(res_1_cohort_2_body), 3, "Should return non empty result for batch 2")
+        self.check_cost_expectation(100, 200, "1st fetch for batch 2, should hit DB to get detail")
         
         # 2nd Request cohort 1
         url = reverse('cohort_scoreboard', args=[1])
@@ -94,9 +90,9 @@ class GetScoreBoardTests(APITestCase):
 
         # 2nd Request cohort 1 Expectation
         res_2_cohort_1_body = json.loads(response_2_cohort_1.content)
-        self.assertEqual(len(res_2_cohort_1_body), 1)
-        self.check_cost_expectation(1, 50)
-        self.assertEqual(res_2_cohort_1_body, res_1_cohort_1_body)
+        self.assertEqual(len(res_2_cohort_1_body), 1, "Should return non empty result for batch 1")
+        self.check_cost_expectation(1, 50, "2nd fetch for batch 1, should hit Cache to get detail")
+        self.assertEqual(res_2_cohort_1_body, res_1_cohort_1_body, "Response from cache and DB should be same")
 
         # 2nd Request cohort 2
         url = reverse('cohort_scoreboard', args=[2])
@@ -104,20 +100,19 @@ class GetScoreBoardTests(APITestCase):
 
         # 2nd Request cohort 2 Expectation
         res_2_cohort_2_body = json.loads(response_2_cohort_2.content)
-        self.assertEqual(len(res_2_cohort_2_body), 3)
-        self.check_cost_expectation(1, 50)
-        self.assertEqual(res_2_cohort_2_body, res_1_cohort_2_body)
+        self.assertEqual(len(res_2_cohort_2_body), 3, "Should return non empty result for batch 1")
+        self.check_cost_expectation(1, 50, "2nd fetch for batch 2, should hit Cache to get detail")
+        self.assertEqual(res_2_cohort_2_body, res_1_cohort_2_body, "Response from cache and DB should be same")
 
-    def test_if_redis_data_flushed_should_rebuild_cache(self):
+    def test_rebuild_cache_when_redis_data_flushed(self):
         url = reverse('cohort_scoreboard', args=[1])
         self.update_score(1, 1, 100)
         response_1 = self.make_new_request(url)
         
         # 1st Request Expectation
         res_1_body = json.loads(response_1.content)
-        self.assertEqual(response_1.status_code, 200)
-        self.assertEqual(len(res_1_body), 1)
-        self.check_cost_expectation(100, 200)
+        self.assertEqual(len(res_1_body), 1, "Should return non empty result")
+        self.check_cost_expectation(100, 200, "should hit DB to get detail")
         
         # Second Request
         redis_client.flushall()
@@ -125,20 +120,26 @@ class GetScoreBoardTests(APITestCase):
 
         # 2nd Request Expectation
         res_2_body = json.loads(response_2.content)
-        self.assertEqual(response_2.status_code, 200)
-        self.assertEqual(len(res_2_body), 1)
-        self.check_cost_expectation(100, 200)
+        self.assertEqual(len(res_2_body), 1, "Should return non empty result, even after redis flushed")
+        self.check_cost_expectation(100, 200, "Should fetch detail from DB if Redis flushed and Rebuild Cache")
 
-    def test_only_allow_30_sec_of_stale_data(self):
+        # 3rd Request
+        response_3 = self.make_new_request(url)
+
+        # 3rd Request Expectation
+        res_3_body = json.loads(response_3.content)
+        self.assertEqual(len(res_3_body), 1, "Should return non empty result, after cache rebuild")
+        self.check_cost_expectation(1, 50, "Should fetch detail from cache, after cache rebuild")
+
+    def test_stale_data_capped_at_30_seconds(self):
         url = reverse('cohort_scoreboard', args=[1])
         self.update_score(1, 1, 100)
         response_1 = self.make_new_request(url)
         
         # 1st Request Expectation
         res_1_body = json.loads(response_1.content)
-        self.assertEqual(response_1.status_code, 200)
         self.assertEqual(len(res_1_body), 1)
-        self.check_cost_expectation(100, 200)
+        self.check_cost_expectation(100, 200, "should hit DB to get detail")
         
         # Second Request
         self.update_score(2, 1, 100)
@@ -146,24 +147,25 @@ class GetScoreBoardTests(APITestCase):
 
         # 2nd Request Expectation
         res_2_body = json.loads(response_2.content)
-        self.assertGreaterEqual(len(res_2_body), 1)
+        self.assertGreaterEqual(len(res_2_body), 1, "Allowed to show stale data for immediate request")
+        self.check_cost_expectation(1, 50, "Should fetch detail from cache")
 
         # 3rd Request after 30sec
-        time.sleep(30)
+        # time.sleep(30)
         response_3 = self.make_new_request(url)
 
         # 3rd Request Expectation
         res_3_body = json.loads(response_3.content)
-        self.assertEqual(len(res_3_body), 2)
+        self.assertEqual(len(res_3_body), 2, "Should return updated scoreboard")
 
     def update_score(self, user_id, cohort_id, score):
         bu = CohortUser.objects.filter(cohort_id=cohort_id, user_id=user_id).first()
         bu.score = score
         bu.save()
 
-    def check_cost_expectation(self, cost_min, cost_max):
-        self.assertLessEqual(get_operation_cost(), cost_max)
-        self.assertGreaterEqual(get_operation_cost(), cost_min)
+    def check_cost_expectation(self, cost_min, cost_max, message=None):
+        self.assertLessEqual(get_operation_cost(), cost_max, message)
+        self.assertGreaterEqual(get_operation_cost(), cost_min, message)
 
     def make_new_request(self, url):
         reset_operation_cost()
